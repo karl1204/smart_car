1517b3fbd7c8a37bd419f28086e0af8ec674345b
karl1204 <39248684+karl1204@users.noreply.github.com>
2018-08-29 11:12:11 +0900

    Update main.py

--- raspberryPi/2jo/new_tech/main.py
+++ raspberryPi/2jo/new_tech/main.py
@@ -9,17 +9,39 @@ GPIO.setup(auto_driving_switch , GPIO.IN)
 GPIO.setup(new_tech_switch , GPIO.IN)
 
 def main():
+    clutchA = newT = allN = isClutchA = isNewT = isAllN = 0
+    
     print ("main code start!")
     #new_tech()
     while True :
         judgeNum = sense.judge()
         if judgeNum == 1:
             if GPIO.input(auto_driving_switch)==1:
-                sense.ClutchAlarm()
+                clutchA++ 
+                #sense.ClutchAlarm()
             elif GPIO.input(new_tech_switch)==1:
-                crush.new_tech()
+                newT++
+                #crush.new_tech()
             else :
-                sense.AllNormal()
+                allN++
+                #sense.AllNormal()
+            if clutch > 10:
+                isClutchA = 1
+                clutchA = newT = allN = isNewT = isAllN = 0
+            elif newT > 10:
+                isNewT = 1
+                clutchA = newT = allN = isClutchA = isAllN = 0
+            elif allN > 10:
+                isAllN = 1
+                clutchA = newT = allN = isClutchA = isNewT = 0
+            else:
+                if isClutchA == 1:
+                    sense.ClutchAlarm()
+                elif isNewT == 1:
+                    crush.new_tech()
+                else:
+                    sense.AllNormal()
+                    
 
 if __name__ == '__main__':
     try:
